7c5558eda601604d246b5d898a6fc98536610ea7
DESKTOP-3RERASQ\Martina <martinavu1@gmail.com>
2023-04-24 22:55:24 +0200

    all done, proud of myself!

--- login.js
+++ login.js
@@ -2,10 +2,7 @@ const loginForm = document.getElementById('login');
 const chatInterface = document.getElementById('chat-interface');
 
 loginForm.addEventListener('submit', event => {
-    // Prevent form submission and page reload
     event.preventDefault(); 
-    // Show chat interface
     chatInterface.style.display = 'block'; 
-    // Hide login form
     loginForm.style.display = 'none'; 
 });
